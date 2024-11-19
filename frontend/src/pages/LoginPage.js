5e1af83024eabd1a2a4a4a55adb338075e6cdf69
Yongjoon Kwon <snailp@DESKTOP-1D000ET.>
2024-11-19 20:15:28 +0900

    [feat]Copoto#8 - added Login Page

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Username:", username);
+    console.log("Password:", password);
+
+    //이 값을 이용해 db를 업데이트하면 됩니다
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+        <h1 className="text-2xl font-bold mb-6 text-center">Copoto</h1>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label className="block text-gray-700 mb-2" htmlFor="username">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="w-full p-2 border border-gray-300 rounded"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full p-2 border border-gray-300 rounded"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full p-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600"
+          >
+            Login
+          </button>
+          <div className = "flex justify-between mt-4">
+            <h3 className = "text-m text-center">계정을 잊으셨나요?</h3>
+            <h3 className = "text-m text-center">새로 오셨나요?</h3>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
