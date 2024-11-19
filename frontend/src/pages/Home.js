1b931982bb75b18f7e83f60b0efd4a3224bc2162
Yongjoon Kwon <snailp@DESKTOP-1D000ET.>
2024-11-19 20:15:09 +0900

    [feat]Copoto#8 - separated home.js from app.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import TopBar from "../components/TopBar.js"
+import SampleSidebar from "../components/SampleSidebar.js"
+
+function Home() {
+    return (
+        <>
+            <TopBar />
+            <SampleSidebar />
+        </>
+    );
+}
+
+export default Home;
