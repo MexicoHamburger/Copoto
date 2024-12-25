de9d95478b05da9a93e71b1b013c86a38c609ba9
Yongjoon PC <littlesnail0513@gmail.com>
2024-12-25 23:00:13 +0900

    [feat]Copoto#10 - Updated Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './index.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
