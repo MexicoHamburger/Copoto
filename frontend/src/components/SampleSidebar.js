fbc5391403f486f9feffbfbb83bc7ec2a9864ec6
Yongjoon PC <littlesnail0513@gmail.com>
2025-01-01 01:14:50 +0900

    [Design]Copoto#14- 메인화면 레이아웃 변경

--- frontend/src/components/SampleSidebar.js
+++ frontend/src/components/SampleSidebar.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
 
-function SampleSidebar() {
-
-  const navigate = useNavigate();
-  const gotoLoginPage = () => {
-    navigate("/login");
-  }
+function SampleSidebar() {  
   return (
-    <div className="flex">
-      <div className="w-1/4 bg-gray-200 h-screen p-4">
-        <h2 className="text-xl font-bold mb-4">SAMPLE SIDEBAR</h2>
-
-        <button
-          onClick = {gotoLoginPage}
-          className="w-full p-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600"
-        >
-          로그인
-        </button>
-      </div>
+    <div className="flex w-1/4 h-screen bg-gray-100 p-4">
     </div>
-
   );
 }
 
--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -1,13 +1,58 @@
 import CopotoLogo from "../images/copotoLogo.png"
+import { useNavigate } from "react-router-dom";
 
 function TopBar() {
+    const navigate = useNavigate();
+    const gotoLoginPage = () => {
+        navigate('/login');
+    }
+    const gotoRegisterPage = () => {
+        navigate('/register');
+    }
+
     return (
-        <div className = "border-b">
+        <header className = "border-b flex">
             <div className = "pl-[10%]">
                 <img src={CopotoLogo} alt = "Copoto Logo"
-                className = "max-w-[200px] p-4"/>
+                className = "max-w-[150px] p-4"/>
             </div>
-        </div>
+            <div className = "w-[15%] flex items-center justify-center text-xl font-bold">
+                <div className = "w-auto px-4 h-auto py-2 rounded-3xl hover:bg-gray-200">
+                    공지사항
+                </div>
+            </div>
+            <div className = "w-[15%] flex items-center justify-center text-xl font-bold">
+                <div className = "w-auto px-4 h-auto py-2 rounded-3xl hover:bg-gray-200">
+                    자유게시판
+                </div>
+            </div>
+            <div className = "w-[15%] flex items-center justify-center text-xl font-bold">
+                <div className = "w-auto px-4 h-auto py-2 rounded-3xl hover:bg-gray-200">
+                    Q&A
+                </div>
+            </div>
+            <div class="w-[15%] flex items-center border border-gray-300 rounded-3xl px-4 my-3 shadow-sm">
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="outline-none w-full text-gray-700 placeholder-gray-400"
+                />
+            </div>
+            <div className="ml-[2%] w-[10%] flex items-center">
+                <button
+                    onClick={gotoLoginPage}
+                    className="w-20 p-2 h-1/2 bg-blue-500 text-white text-xs font-bold rounded-3xl hover:bg-blue-600"
+                >
+                    로그인
+                </button>
+                <button
+                    onClick={gotoRegisterPage}
+                    className="w-20 ml-5 p-2 h-1/2 bg-gray-200 text-blue-500 text-xs font-bold rounded-3xl hover:bg-gray-300"
+                >
+                    회원가입
+                </button>
+            </div>
+        </header>
     )
 }
 
