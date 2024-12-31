182eda85817b713f128dc442b246b6b4bbb597e0
Yongjoon PC <littlesnail0513@gmail.com>
2024-12-31 17:39:42 +0900

    [Design]Copoto#14 - 상단바 디자인 변경

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -2,9 +2,11 @@ import CopotoLogo from "../images/copotoLogo.png"
 
 function TopBar() {
     return (
-        <div className = "border-2">
-            <img src={CopotoLogo} alt = "Copoto Logo"
-            className = "max-w-[200px] p-4"/>
+        <div className = "border-b">
+            <div className = "pl-[10%]">
+                <img src={CopotoLogo} alt = "Copoto Logo"
+                className = "max-w-[200px] p-4"/>
+            </div>
         </div>
     )
 }
