71d72a964956aa63dacf4b87d357a3472e0732b0
Yongjoon PC <littlesnail0513@gmail.com>
2025-01-01 20:44:11 +0900

    [feat]Copoto#16 - removed unused sidebar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,9 @@
 import TopBar from "../components/TopBar.js"
-import SampleSidebar from "../components/SampleSidebar.js"
 
 function Home() {
     return (
         <>
             <TopBar />
-            <div className = "pl-[10%]">
-                <SampleSidebar />
-            </div>
         </>
     );
 }
